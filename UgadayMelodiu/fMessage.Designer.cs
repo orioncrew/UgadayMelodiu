0cddf4e6787a19d3f8df9a7e0be1916aa6a7c233
orioncrew <orioncrew@yandex.ru>
2018-11-22 21:27:09 +0500

    Новая форма

---
+++ UgadayMelodiu/fMessage.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace UgadayMelodiu
+{
+    partial class fMessage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 53);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.button1.Location = new System.Drawing.Point(21, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Да";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.button2.Location = new System.Drawing.Point(189, 416);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 62);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Нет";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // fMessage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 545);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "fMessage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.Label label1;
+    }
+}
---
+++ UgadayMelodiu/fMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UgadayMelodiu
+{
+    public partial class fMessage : Form
+    {
+        public fMessage()
+        {
+            InitializeComponent();
+        }
+    }
+}
